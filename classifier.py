993fc96539213b837ce048e2207f46781bb37973
Sinclair Hudson <hudson.sinclair00@gmail.com>
2021-01-16 07:50:08 -0500

    rounding out training

--- classifier.py
+++ classifier.py
@@ -3,5 +3,31 @@ import torch.nn as nn
 
 class RadarDroneClassifier(nn.Module):
     def __init__(self):
+        super(RadarDroneClassifier, self).__init__()
+        self.conv1 = nn.Conv2d(1,5, (1, 7))  # conv1D, effectively
+        self.conv2 = nn.Conv2d(5,10, (5, 5))
+        self.maxpool = nn.MaxPool2d((2,2), stride=(2,2))
+
+        self.conv3 = nn.Conv2d(10, 6, (3, 3))
+        self.conv4 = nn.Conv2d(10, 6, (3, 3))
+
+        self.conv5 = nn.Conv2d(16, 6, (3, 3))
+
+        self.linear = nn.Linear(something, 5)
+
+        self.drop = nn.Dropout2D(p=0.5)
+        self.relu = nn.LeakyReLU()
+
+        self.LN = nn.LayerNorm(size[1:])
 
     def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.drop(x)
+        x1 = self.relu(self.LN(self.conv2(x)))
+        x = self.relu(self.conv3(x1))
+        x = self.drop(x)
+        x = self.relu(self.conv4(x))
+        x = self.relu(self.conv5(torch.cat((x1, x), dim=1)))  # the skip connection
+        x = x.reshape((-1, ))
+        x = self.linear(x)
+        return x
--- train.py
+++ train.py
@@ -1,15 +1,48 @@
 import torch.nn
+import time
 import wandb
 import torchvision
 import torchvision.datasets as ds
 import numpy as np
+from classifier import RadarDroneClassifier
 
-def Xdataloader(file_extension):
+c = {
+    "epochs": 5,
+    "learning_rate": 0.001,
+}
+
+wandb.init(project="cansofcom", config=c)
+
+def dataloader(file_extension):
     data = np.load(file_extension)
-    data = torch.Tensor(data)
-    data = torchvision.transforms.Normalize((-24,), (50,))(data)
     return data
 
-def Wdataloader(file_extension):
+Wband0SNR = ds.DatasetFolder("dataset/26000fs/0SNR")
 
-Wband0SNR = ds.DatasetFolder("")
+net = RadarDroneClassifier()
+
+optim = torch.optim.AdamW(net.parameters(), lr=c["learning_rate"])
+criterion = nn.CrossEntropyLoss()
+
+for x in range(c["epochs"]):
+    for i, data in enumerate(trainLoader):
+
+        inputs, y = data
+        optim.zero_grad()
+
+        start = time.time()
+        yhat = net(inputs)
+        middle = time.time()
+        loss = criterion(yhat, y)
+        loss.backward()
+        end = time.time()
+        optim.step()
+
+        wandb.log({
+            "loss": loss.item(),
+            "forward_time": middle - start,
+            "backward_time": end - middle,
+            "accuracy": end - middle,
+        })
+
+
